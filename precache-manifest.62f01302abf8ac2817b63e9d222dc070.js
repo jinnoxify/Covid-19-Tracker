33175b0f20d78d94debb8bfc3c0d424b5abc17ba
jinnoxify <oscarpereznavarro1997@gmail.com>
2020-10-21 21:06:43 -0500

    Updates

---
+++ precache-manifest.62f01302abf8ac2817b63e9d222dc070.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00ed1bb5ff5b99b1a52915aff2f6e5f6",
+    "url": "/Covid-19-Tracker/index.html"
+  },
+  {
+    "revision": "8fe39e5dfa6bdbb4cd74",
+    "url": "/Covid-19-Tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "8ed07f0ceee59e5eda11",
+    "url": "/Covid-19-Tracker/static/css/main.fe94a663.chunk.css"
+  },
+  {
+    "revision": "8fe39e5dfa6bdbb4cd74",
+    "url": "/Covid-19-Tracker/static/js/2.9ce3b531.chunk.js"
+  },
+  {
+    "revision": "af5492f10234ec8ba29a624c4fe6fb11",
+    "url": "/Covid-19-Tracker/static/js/2.9ce3b531.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ed07f0ceee59e5eda11",
+    "url": "/Covid-19-Tracker/static/js/main.49d51a1a.chunk.js"
+  },
+  {
+    "revision": "e53de683d501869629f6",
+    "url": "/Covid-19-Tracker/static/js/runtime-main.af13cdb2.js"
+  }
+]);
